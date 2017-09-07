e0e01c8e500fb20267d6d733a75b5bd9469a463e
Derek Rhodes <physci@gmail.com>
2017-09-06 21:28:34 -0400

    adds graphviz backend to help debug, it compiles, but not useful yet.

--- app/Main.hs
+++ app/Main.hs
@@ -4,39 +4,75 @@ import Jade.Types
 import Options.Applicative
 import Data.Semigroup ((<>))
 import Control.Monad
+import qualified Jade.Back.Viz as Viz
+import qualified Jade.Decode as Decode
+import qualified Data.Text.IO as TIO
 
-data Example = Example { infile :: String
-                       , hdl :: String
-                       , enthusiasm :: Int
-                       }
+data Opts = Opts { infile :: String
+                 , topLevelMod :: String
+                 , viz :: Bool
+                 , hdlType :: Maybe String
+                 } deriving (Show, Eq)
 
-targetOption :: Parser String
-targetOption = strOption $ mconcat [ long "target-lang"
-                                   , short 't'
-                                   , metavar "TARGETLANG"
-                                   , help "[vhdl|verilog]" ]
 
 infileOption = strOption $ mconcat [ long "infile"
-                                   , metavar "SOURCE"
-                                   , help "JADE exported json module" ]
+                                   , metavar "SOURCEPATH"
+                                   , help "JADE exported json module"
+                                   ]
 
+topLevelModArg = strOption $ mconcat [ long "toplevel-mod"
+                                     , metavar "STRING"
+                                     , help "the name of the top level entity"
+                                     ]
 
-thuseOption = option auto $ mconcat [ long "enthusiasm"
-                                    , help "How enthusiastically to greet"
-                                    , showDefault
-                                    , value 1
-                                    , metavar "INT" ]
+vizOption = switch $ mconcat [ long "viz"
+                             , short 'z'
+                             , help "emit graphviz of the jade module"
+                             ]
 
-sample :: Parser Example
-sample = liftA3 Example targetOption infileOption thuseOption
+hdl = strOption $ mconcat [ long "hdl"
+                          , metavar "[vhdl|verilog]"
+                          , help "HDL language, vhdl or verilog"
+                          ]
+
+sample :: Parser Opts
+sample = Opts
+         <$> infileOption
+         <*> topLevelModArg
+         <*> vizOption
+         <*> optional hdl 
   
 main :: IO ()
-main = greet =<< execParser opts
+main = execParser opts >>= greet
   where
     opts = info (sample <**> helper)
       ( fullDesc
-     <> progDesc "Print a greeting for TARGET"
-     <> header "hello - a test for optparse-applicative" )
+        <> progDesc "a JADE module coverter from schematic to HDL"
+        <> header "hello - not sure what this string goes to")
 
-greet :: Example -> IO ()
-greet (Example h s n) = putStrLn $ "Hello, " ++ h ++ replicate n '!'
+greet :: Opts -> IO ()
+greet opts = do
+  --formatOptions opts
+  generateViz opts
+
+formatOptions opts = do
+  putStrLn $ "Jade module   :  " ++ (infile opts)
+  putStrLn $ "graph viz?    :  " ++ (show $ viz opts)
+  case hdlType opts of
+    Just s -> putStrLn $ "HDL           :  " ++ s
+    Nothing -> putStrLn $ "HDL           :  " ++ "None"
+
+generateViz opts = when (viz opts) $ do
+  -- putStrLn "Emitting graphviz dot file!!"
+  -- putStrLn "Well... not yet.. "
+
+  topl <- Decode.decodeTopLevel $ infile opts
+  
+  let dotSrc = case topl of
+        Left msg -> error msg
+        Right topl -> runJ topl $ do
+          Viz.mkAllMods (topLevelMod opts)
+
+  case dotSrc of
+    Left msg -> putStrLn msg
+    Right src -> TIO.putStrLn src
