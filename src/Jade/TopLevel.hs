ddc22cb7b55ea5aeff1f1ab5b547daea6532423e
Derek Rhodes <physci@gmail.com>
2017-09-27 20:24:48 -0400

    cleanup

--- src/Jade/TopLevel.hs
+++ src/Jade/TopLevel.hs
@@ -26,25 +26,25 @@ module Jade.TopLevel ( getNetsWithName
 
 import Control.Monad
 import Control.Monad.State
+import Data.Maybe
+import Jade.Common
 import qualified Data.List as DL
 import qualified Data.Map as DM
-import Data.Maybe
 import qualified Data.Maybe as Maybe
 import qualified Data.Set as DS
 import qualified Data.Vector as DV
+import qualified Jade.Decode.Bundle as Bundle
 import qualified Jade.Decode.Decode as D
 import qualified Jade.Jumper as Jumper
 import qualified Jade.MemUnit as MemUnit
 import qualified Jade.Module as Module
 import qualified Jade.Net as Net
 import qualified Jade.Part as Part
-import qualified Jade.Term as Term
 import qualified Jade.Schematic as Schem
-import Jade.Common
+import qualified Jade.Term as Term
 import qualified Jade.UnionFindST as UF
 import qualified Jade.Wire as Wire
 import qualified Web.Hashids as WH
-import qualified Jade.Decode.Bundle as Bundle
 
 getSubModules :: String -> J [SubModule]
 getSubModules modname = do --"TopLevel.getSubModules" <? do
@@ -314,13 +314,11 @@ replicationDepth :: String -> SubModule -> J Int
 replicationDepth modname submod  = "TopLevel.replicationDepth" <? do
   terms <- terminals submod
   ratios <- concatMapM (getRatios modname) terms
-  let numReps = [if tw >= pw then 1 else pw `div` tw | (tw, pw) <- ratios]
-  return $ maximum numReps
+  return $ maximum [if tw >= pw then 1 else pw `div` tw | (tw, pw) <- ratios]
 
 getNetsWithName :: String -> String -> J [Net]
 getNetsWithName modname signame = "TopLevel.getNetsWithName" <? do
-  ns <- nets modname
-  filterM (flip Net.containsIdent signame) ns
+  filterM (flip Net.containsIdent signame) =<< nets modname
   
 getAllSigNames :: String -> J [String]
 getAllSigNames modname = do
